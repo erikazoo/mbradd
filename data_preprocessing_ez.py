18f4361e9e84428760530c89a29e44cbcae687e9
erikazoo <ericaz@mtu.edu>
2026-09-21 19:44:24 -0400

    TODO: add data loading and cleaning cells.

---
+++ data_preprocessing_ez.py
@@ -0,0 +1,34 @@
+# /// script
+# requires-python = ">=3.14"
+# dependencies = [
+#     "marimo>=0.23.3",
+#     "pandas>=3.0.6",
+#     "scikit-learn>=1.9.1",
+# ]
+# ///
+
+import marimo
+
+__generated_with = "0.24.2"
+app = marimo.App()
+
+
+@app.cell
+def _():
+    import marimo, pandas, sklearn
+
+    return
+
+
+@app.cell
+def _():
+    return
+
+
+@app.cell
+def _():
+    return
+
+
+if __name__ == "__main__":
+    app.run()
